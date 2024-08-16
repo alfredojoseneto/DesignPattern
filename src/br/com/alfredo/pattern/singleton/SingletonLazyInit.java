daa479a2c37722b6166ad4bd398ff47de184b60f
Alfredo Neto <alfredojoseneto@gmail.com>
2024-08-16 07:05:28 -0300

    ADD: singleton lazy init

---
+++ src/br/com/alfredo/pattern/singleton/SingletonLazyInit.java
@@ -0,0 +1,22 @@
+package br.com.alfredo.pattern.singleton;
+
+public class SingletonLazyInit {
+	
+	private volatile static SingletonLazyInit uniqueInstance;
+	
+	private SingletonLazyInit() {}
+	
+	
+	private static SingletonLazyInit getInstance() {
+		if(uniqueInstance == null) {
+			synchronized(SingletonLazyInit.class) {
+				if(uniqueInstance == null) {
+					uniqueInstance = new SingletonLazyInit();
+				}
+			}
+		}
+		return uniqueInstance;
+			
+	}
+
+}
